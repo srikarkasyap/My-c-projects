b2276b9cea312ec5561aa6e6f465169b9bb64efc
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2019-12-29 10:10:31 +0530

    Add files via upload
    A 'c' program to tell weather the given year is leap year or not.

---
+++ Leapyear or not.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+int a;				
+	printf("Enter a year");			
+	scanf("%d",&a);		
+	
+	if(a%400==0){
+	printf("It's a leapyear");
+}
+	else if(a%100){
+	printf("It'a leapyear");
+}
+	else if(a%4==0){
+	printf("It's a leapyer");
+}
+	else{
+	printf("Enter a proper year");
+}
+}
