98bc430e70cc7b9ec2bc8afe5a3e1e6889bdaeeb
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2019-12-29 10:44:11 +0530

    Add files via upload

---
+++ sum of n natrl no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int n,a,d,s;
+	printf("Enter nth natural number:");
+	scanf("%d",n);
+d=1;
+a=1;
+	s=n/2*(2*a+(n-1)d)
+ 
+ 	printf("%d",s);
+
+clrscr();
+
+}
