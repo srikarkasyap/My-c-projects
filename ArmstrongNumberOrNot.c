54ac21841e7048317118487a83a7f7aedd6d0154
Kasyap <49114820+srikarkasyap@users.noreply.github.com>
2020-01-13 18:50:23 +0530

    Add files via upload

---
+++ ArmstrongNumberOrNot.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){ 
+int k,b,g[50],c,d,y,x=0,z,q=0;
+	printf("Enter a number:");
+	scanf("%d",&k);
+	y=k;
+	while(k!=0){
+	
+
+	b=k;
+	k=k/10;
+	d=k*10;
+    c=b-d;
+    g[x]=c;    
+    x++;
+ 
+}
+
+    z=x;
+    while(x--){
+    q=q+pow(g[x],z);
+}
+
+
+	if(y==q){
+	
+	printf("It's a armstrong number.\n");		
+		
+	}
+	else{
+
+	printf("It is'nt a armstrong number.\n");
+}
+}
+
